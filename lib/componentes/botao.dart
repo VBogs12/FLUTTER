ff439f32fdc937c147e6c88b83f750ba7f26a602
VBogs12 <125492186+VBogs12@users.noreply.github.com>
2023-05-10 16:06:29 -0300

    Update botao.dart

--- lib/componentes/botao.dart
+++ lib/componentes/botao.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Botao extends StatefulWidget {
   final texto;
   final corBackground;
-  final corFonte;
+  final corFont;
   final funcao;
 
   const Botao(
